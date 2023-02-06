9971443b769b3d66e0af73c413ffe8b74b825d80
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-06 12:16:46 -0800

    Image optimization (#46)
    * add image component to pages
    
    * add in the actual component
    
    * add in vercel adapter
    
    * trigger rebuild
    
    * add in vercel config file
    
    * add in version 3
    
    * add in more output configuration
    
    * remove old config
    
    * remove routes for now
    
    * add in better width to image
    
    * add in production check
    
    * use larger image for headers
    
    * remove width on code blocks
    
    * minor style update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import markdownIntegration from "@astropub/md";
+import vercel from '@astrojs/vercel/static';
 import rehypeLinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import { remarkReadingTime } from "./scripts/remark-reading-time.mjs";
@@ -12,6 +13,21 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jcbl.ws",
+  output: "static",
+  outDir: './.vercel/output/static',
+  adapter: vercel({
+    "images": {
+      "sizes": [640, 750, 828, 1080, 1200],
+      "domains": [],
+      "minimumCacheTTL": 60,
+      "formats": ["image/avif", "image/webp"],
+      "remotePatterns": [{
+        "protocol": "https",
+        "hostname": "^avatars1\\.githubusercontent\\.com$",
+        "pathname": "^/u/578259\\?s=460&v=4$"
+      }]
+    }
+  }),
   integrations: [
     mdx(),
     sitemap(),
