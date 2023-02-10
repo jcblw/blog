8ff6df11c6fe3500021da6d16e33cef96c972b87
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-10 08:24:37 -0800

    add in some additional styling, and also a post about astro vercel image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,62 +1,59 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import markdownIntegration from "@astropub/md";
-import vercel from '@astrojs/vercel/static';
-import rehypeLinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
-import { remarkReadingTime } from "./scripts/remark-reading-time.mjs";
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import vercelImage from 'astro-vercel-image'
+import react from '@astrojs/react'
+import markdownIntegration from '@astropub/md'
+import rehypeLinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import { remarkReadingTime } from './scripts/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jcbl.ws",
-  output: "static",
-  outDir: './.vercel/output/static',
-  adapter: vercel({
-    "images": {
-      "sizes": [640, 750, 828, 1080, 1200],
-      "domains": [],
-      "minimumCacheTTL": 60,
-      "formats": ["image/avif", "image/webp"],
-      "remotePatterns": [{
-        "protocol": "https",
-        "hostname": "^avatars1\\.githubusercontent\\.com$",
-        "pathname": "^/u/578259\\?s=460&v=4$"
-      }]
-    }
-  }),
+  site: 'https://jcbl.ws',
+  output: 'static',
   integrations: [
     mdx(),
     sitemap(),
     react(),
     markdownIntegration(),
     sitemap({
-      changefreq: "weekly",
+      changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
+    }),
+    vercelImage({
+      sizes: [640, 750, 828, 1080, 1200],
+      domains: [],
+      minimumCacheTTL: 60,
+      formats: ['image/avif', 'image/webp'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '^avatars1\\.githubusercontent\\.com$',
+          pathname: '^/u/578259\\?s=460&v=4$',
+        },
+      ],
     }),
   ],
   markdown: {
     extendDefaultPlugins: true,
-    syntaxHighlight: "prism",
+    syntaxHighlight: 'prism',
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
       rehypeSlug,
       [
         rehypeLinkHeadings,
         {
-          behavior: "prepend",
+          behavior: 'prepend',
           content: {
-            type: "element",
-            tagName: "span",
-            properties: { className: ["heading-link"] },
-            children: [{ type: "text", value: "⌗" }],
+            type: 'element',
+            tagName: 'span',
+            properties: { className: ['heading-link'] },
+            children: [{ type: 'text', value: '⌗' }],
           },
         },
       ],
     ],
   },
-});
+})
