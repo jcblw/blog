29be325cc4823dfe850917fa89d82667eec6bce6
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-10 09:29:25 -0800

    remove outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ import { remarkReadingTime } from './scripts/remark-reading-time.mjs'
 export default defineConfig({
   site: 'https://jcbl.ws',
   output: 'static',
-  outDir: './.vercel/output/static',
   integrations: [
     mdx(),
     react(),
