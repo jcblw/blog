a6f1494d74587a8d079c13a30d82e52f692bc37c
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-15 08:09:08 -0800

    add in small images

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
       lastmod: new Date(),
     }),
     vercelImage({
-      sizes: [640, 750, 828, 1080, 1200],
+      sizes: [8, 640, 750, 828, 1080, 1200],
       domains: [],
       minimumCacheTTL: 60,
       formats: ['image/avif', 'image/webp'],
