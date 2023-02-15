d4186072d668efc869b767629b7aaac2085dd2b5
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-15 08:01:28 -0800

    add in lazy loading img component

---
+++ src/components/Img.tsx
@@ -0,0 +1,70 @@
+import type { HTMLAttributes } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+
+export interface Props extends HTMLAttributes<HTMLImageElement> {
+  src: string
+  alt: string
+  width?: number
+  quality?: number
+}
+
+const isBuild = import.meta.env.MODE === 'production'
+
+export const Img = ({
+  src,
+  width,
+  quality,
+  alt,
+  className,
+  ...rest
+}: Props) => {
+  const [isLoaded, setIsLoaded] = useState(!isBuild)
+  const { optimizedURL, shouldBlur, placeholderURL } = useMemo(() => {
+    if (isBuild) {
+      return {
+        shouldBlur: true,
+        placeholderURL: `/_vercel/image?url=${encodeURIComponent(
+          src
+        )}&w=8&q=50`,
+      }
+    } else {
+      return {
+        shouldBlur: false,
+        placeholderURL: '',
+        optimizedURL: src,
+      }
+    }
+  }, [width, quality, src])
+
+  useEffect(() => {
+    if (shouldBlur && !isLoaded) {
+      const img = new Image()
+      img.src = src
+      img.onload = () => {
+        setIsLoaded(true)
+      }
+    }
+  }, [src, shouldBlur])
+
+  return (
+    <div className={className} style={width ? { width: `${width}px` } : {}}>
+      {shouldBlur && !isLoaded ? (
+        <img
+          src={placeholderURL}
+          alt={alt}
+          width={`${width}px`}
+          className={className}
+          {...rest}
+        />
+      ) : (
+        <img
+          src={optimizedURL}
+          alt={alt}
+          width={`${width}px`}
+          className={className}
+          {...rest}
+        />
+      )}
+    </div>
+  )
+}
