6482245e073b4cb4c35ba5703c33090e1988b431
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-04-08 15:40:04 -0700

    add in yarn lock update for package, and make middleware redirect

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,16 @@
-export default function middleware(_request, _event) {
+/**
+ * Edge middleware, I set this up so we can do a redirect from blog posts with a trailing slash
+ * to the same post without a trailing slash. This was to resolve some issues with Google indexing
+ */
+export default function middleware(request, _event) {
   const response = new Response()
-
-  // Set custom header
-  response.headers.set('x-modified-edge', 'true')
-
-  // "Pass through" the middleware to complete the HTTP request
-  response.headers.set('x-middleware-next', '1')
+  const url = new URL(request.url)
+  const { pathname } = url
+  const hasTrailingSlash = pathname.endsWith('/')
+  if (hasTrailingSlash) {
+    response.status = 301
+    response.headers.set('Location', pathname.slice(0, -1))
+  }
 
   return response
 }
