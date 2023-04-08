ba89f48d9ce50cec5562fc8af65479897816d405
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-04-08 09:08:14 -0700

    strip away trailing slashes on sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,11 @@ export default defineConfig({
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
+      // serialize function should strip away trailing slashes on urls to match canonical urls
+      serialize: ({ url, ...otherProps }) => ({
+        url: url.replace(/\/$/, ''),
+        ...otherProps,
+      }),
     }),
     vercelImage({
       sizes: [640, 750, 828, 1080, 1200],
