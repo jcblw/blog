fb1921b2df57bad737fb0412d089a8eeee88f8f7
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-04-08 16:18:22 -0700

    Edge function for redirects  (#70)
    * add in config
    
    * add in sample middleware
    
    * add in yarn lock update for package, and make middleware redirect
    
    * revise how we are redirecting
    
    * add in edge package to do rewrite
    
    * remove edge package and just bring in function
    
    * add in path name
    
    * use location
    
    * bring in some more code around rewrite
    
    * remove 301 redirect for edge function
    
    * attempt to use location again

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+function rewrite(destination, init) {
+  const headers = new Headers({})
+  headers.set('location', String(destination))
+  return new Response(null, {
+    ...init,
+    headers,
+  })
+}
+/**
+ * Edge middleware, I set this up so we can do a redirect from blog posts with a trailing slash
+ * to the same post without a trailing slash. This was to resolve some issues with Google indexing
+ */
+export default function middleware(request, _event) {
+  const url = new URL(request.url)
+  const { pathname } = url
+  const hasTrailingSlash = pathname.endsWith('/')
+  const hasPathname = pathname !== '/'
+  if (hasTrailingSlash && hasPathname) {
+    return rewrite(new URL(pathname.slice(0, -1), url.origin), { status: 301 })
+  }
+}
