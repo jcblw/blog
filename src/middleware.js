ba3768181f16b54b9a611e438b41eee983bcf79c
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-04-08 15:51:34 -0700

    add in edge package to do rewrite

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,14 @@
+import { rewrite } from '@vercel/edge'
+
 /**
  * Edge middleware, I set this up so we can do a redirect from blog posts with a trailing slash
  * to the same post without a trailing slash. This was to resolve some issues with Google indexing
  */
 export default function middleware(request, _event) {
-  const response = new Response()
   const url = new URL(request.url)
   const { pathname } = url
   const hasTrailingSlash = pathname.endsWith('/')
   if (hasTrailingSlash) {
-    const redirectUrl = new URL(pathname.slice(0, -1), url.origin)
-    response.redirect(redirectUrl, 301)
+    return rewrite(new URL(pathname.slice(0, -1), url.origin), { status: 301 })
   }
-
-  return response
 }
