dd1498a71c92176cfb8d92cfe9178df986308ca5
Jacob Lowe <jacoblowe2.0@gmail.com>
2024-05-25 18:06:13 -0700

    cleanup script, and redesign recent section

--- scripts/create-embeddings.js
+++ scripts/create-embeddings.js
@@ -2,15 +2,16 @@ import fs from 'node:fs/promises'
 import 'dotenv/config'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-const dirname = path.dirname(fileURLToPath(import.meta.url))
-
 import { sql } from '@vercel/postgres'
-
-
 import { embed } from 'ai'
 import { openai } from '@ai-sdk/openai'
 
-const SKIP_EMBEDDINGS = false
+const args = process.argv.slice(2)
+
+const shouldRegenerate = args.includes('--regenerate')
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
 
 /**
  * @type {(folder: string) => (file: string) => string}
@@ -18,20 +19,6 @@ const SKIP_EMBEDDINGS = false
 const resolveContent = (folder) => (file) =>
   path.resolve(dirname, '../src/content', folder, file)
 
-/**
- * cosine similarity checks the similarity between two vectors
- * @type {(a: number[], b: number[]) => number}
- */
-const cosineSimilarity = (a, b) => {
-  if (a.length !== b.length) {
-    throw new Error('Vectors must be of the same length')
-  }
-
-  const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0)
-  const magnitudeA = Math.sqrt(a.reduce((acc, val) => acc + val ** 2, 0))
-  const magnitudeB = Math.sqrt(b.reduce((acc, val) => acc + val ** 2, 0))
-  return dotProduct / (magnitudeA * magnitudeB)
-}
 
 /**
  * @type {(x: any) => x is number[]}
@@ -72,7 +59,9 @@ const isOneDimensionalArray = (x) =>
   const files = [
     ...blogPosts.map((f) => resolveBlog(f)),
     ...videoPosts.map((f) => resolveVideo(f)),
-  ].filter((file) => file.endsWith('.mdx'))
+  ].filter((file) => file.endsWith('.md') || file.endsWith('.mdx'))
+
+  console.log(`Found ${files.length} files...`)
 
   await Promise.all(
     files.map(async (file) => {
@@ -95,15 +84,33 @@ const isOneDimensionalArray = (x) =>
     })
   )
 
+  if (shouldRegenerate) {
+    // clear the database
+    await sql`DELETE FROM Post`
+  }
+
+  const { rows: generatedEmbeddings } = await sql`
+    SELECT slug FROM Post WHERE embeddings IS NOT NULL
+  `;
+  const filesWithoutEmbeddings = files.filter(file => {
+    const fileCache = cache.get(file)
+    if (!fileCache || !fileCache.slug) {
+      return false
+    }
+    return !generatedEmbeddings.some(({ slug }) => slug === fileCache.slug)
+  })
+
+  console.log(`Generating embeddings for ${filesWithoutEmbeddings.length} files...`)
+
   /**
    * generate embeddings for each file in the content directory
    * and store it in the cache
    *
-   * This runs sequentially to avoid slamming the huggingface API
+   * This runs sequentially to avoid slamming the API
    *
    * @type {Promise<void>}
    */
-  await files.reduce(async (promise, file) => {
+  await filesWithoutEmbeddings.reduce(async (promise, file) => {
     return promise.then(async () => {
       const fileCache = cache.get(file)
       if (!fileCache) {
@@ -119,9 +126,7 @@ const isOneDimensionalArray = (x) =>
           value: content,
         })
       ).embedding
-      // console.log({ embeddings })
       if (isOneDimensionalArray(embeddings)) {
-        console.log(`Embeddings generated for ${file}!`)
         cache.set(file, { ...fileCache, embeddings })
         const embeddingsString = `[${embeddings.join(',')}]`
         await sql`
@@ -133,56 +138,4 @@ const isOneDimensionalArray = (x) =>
   }, Promise.resolve())
 
   console.log('Embeddings generated! 🚀')
-  console.log('Calculating cosine similarities...')
-
-  /**
-   * calculate cosine similarity between embeddings,
-   * and write the results to a file. the format of the file would be:
-   * {
-   *  "file1": [{ similarity: 0.1, file: "file2" }], // cosine similarity with other files
-   * }
-   */
-  const similarities = files.reduce((acc, file, i) => {
-    const fileCache = cache.get(file)
-    console.log({ fileCache })
-    if (!fileCache?.embeddings || !fileCache?.slug) {
-      console.log(`Skipping ${file}...`)
-      return acc
-    }
-    const slug = fileCache?.slug
-    const embeddings = fileCache.embeddings
-    const similarities = files
-      .map((e) => {
-        const _cache = cache.get(e)
-        if (!_cache?.embeddings || _cache?.isDraft) {
-          return null
-        }
-        return {
-          similarity: cosineSimilarity(embeddings, _cache.embeddings),
-          slug: _cache?.slug,
-          title: _cache?.title,
-          description: _cache?.description,
-          heroImage: _cache?.heroImage,
-        }
-      })
-      .filter((x) => !!x)
-      .sort((a, b) => b.similarity - a.similarity)
-      .slice(1, 4)
-    return {
-      ...acc,
-      [slug]: similarities,
-    }
-  }, {})
-
-  console.log(similarities)
-
-  cache.clear()
-  if (SKIP_EMBEDDINGS) {
-    return
-  }
-  const similaritiesFile = path.resolve(dirname, '../similarities.json')
-  await fs.writeFile(similaritiesFile, JSON.stringify(similarities, null, 2))
-
-  console.log('Cosine similarities calculated! 🚀')
-  console.log(`Similarities written to ${similaritiesFile}`)
 })()
--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -6,10 +6,10 @@ import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ params }) => {
 
-const query = params['query']
-const posts = query ? await searchPosts(query) : []
+  const query = params['query']
+  const posts = query ? await searchPosts(query) : []
 
-console.log(query, posts)
+  console.log(query, posts)
   return new Response(
     JSON.stringify({
       query,
