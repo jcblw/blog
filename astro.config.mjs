928611b32477d298a7ee112e0eccd20ea22068a2
Jacob Lowe <jacoblowe2.0@gmail.com>
2024-06-01 08:02:21 -0700

    add in a bunch of upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,77 +1,53 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import sitePreset from '@jcblw/astro-site-integration'
-import react from '@astrojs/react'
-import markdownIntegration from '@astropub/md'
-import rehypeLinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
-import { remarkReadingTime } from './scripts/remark-reading-time.mjs'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import sitePreset from '@jcblw/astro-site-integration';
+import react from '@astrojs/react';
+import markdownIntegration from '@astropub/md';
+import rehypeLinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import { remarkReadingTime } from './scripts/remark-reading-time.mjs';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jcbl.ws',
-  output: 'static',
+  output: 'hybrid',
   trailingSlash: 'never',
-  integrations: [
-    mdx(),
-    react(),
-    markdownIntegration(),
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      // serialize function should strip away trailing slashes on urls to match canonical urls
-      serialize: ({ url, ...otherProps }) => ({
-        url: url.replace(/\/$/, ''),
-        ...otherProps,
-      }),
-      filter: (page) => !page.includes('draft'),
+  integrations: [mdx(), react(), markdownIntegration(), sitemap({
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date(),
+    // serialize function should strip away trailing slashes on urls to match canonical urls
+    serialize: ({
+      url,
+      ...otherProps
+    }) => ({
+      url: url.replace(/\/$/, ''),
+      ...otherProps
     }),
-    sitePreset({
-      middleware: './middleware.js',
-      sizes: [640, 750, 828, 1080, 1200],
-      domains: [],
-      minimumCacheTTL: 60,
-      formats: ['image/avif', 'image/webp'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '^avatars1\\.githubusercontent\\.com$',
-          pathname: '^/u/578259\\?s=460&v=4$',
-        },
-      ],
-    }),
-    tailwind(),
-  ],
+    filter: page => !page.includes('draft')
+  }), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
     syntaxHighlight: 'prism',
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeLinkHeadings,
-        {
-          behavior: 'prepend',
-          content: {
-            type: 'element',
-            tagName: 'span',
-            properties: {
-              className: ['heading-link'],
-            },
-            children: [
-              {
-                type: 'text',
-                value: '⌗',
-              },
-            ],
-          },
+    rehypePlugins: [rehypeSlug, [rehypeLinkHeadings, {
+      behavior: 'prepend',
+      content: {
+        type: 'element',
+        tagName: 'span',
+        properties: {
+          className: ['heading-link']
         },
-      ],
-    ],
+        children: [{
+          type: 'text',
+          value: '⌗'
+        }]
+      }
+    }]]
   },
-})
+  adapter: vercel()
+});
